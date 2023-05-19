670568d34f92b6233441347fda8f365c549aa7a9
replyre <121796450+replyre@users.noreply.github.com>
2023-05-20 04:12:24 +0530

    updated fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { Button, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import RecipeTile from "./RecipeTile";
 import BrunchDiningIcon from "@mui/icons-material/BrunchDining";
+import axios from "axios";
 const Home = () => {
   const [recipeQuery, setRecipeQuery] = React.useState(""); // for searching of ingredients, dishes etc.
   const [Recipes, setRecipes] = React.useState([]); // getting the recipes of searched item
@@ -18,13 +19,13 @@ const Home = () => {
   const YOUR_APP_ID = "1ff928e6";
   const YOUR_APP_KEY = "360b99901303560e4f0fdd0d07f920a8";
   // API URL
-  var url = `https://api.edamam.com/search?q=${recipeQuery}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=10&calories=591-722&health=alcohol-free`;
+  var url = `https://api.edmam.com/search?q=${recipeQuery}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=10&calories=591-722&health=alcohol-free`;
   // functon to fetch data from API
   async function getRecipes() {
-    const response = await fetch(url);
-    const jsonData = await response.json();
-
-    setRecipes(jsonData.hits);
+    await axios
+      .get(url)
+      .then((res) => setRecipes(res.data.hits))
+      .catch((err) => alert(err));
   }
   return (
     <div className="home">
